062de9f248c60cb752d8b518066a5c72807469dc
Valentin <valentin_00794@bk.ru>
2022-08-18 09:49:09 +0300

    Фикс замечаний наставника по предыдущей ДЗ

--- src/view/edit-point-view.js
+++ src/view/edit-point-view.js
@@ -6,21 +6,7 @@ import { mockOffersByType } from '../mock/offers.js';
 
 const editPointTemplate = (point) => {
   const { dateFrom, dateTo, type, destination, basePrice, offers } = point;
-
   const isOfferChecked = (offer) => offers.includes(offer.id) ? 'checked' : '';
-
-  const getDestinationNameAndDecription = () => {
-    let name = '';
-    let description = '';
-    for (const dest of destinations) {
-      if (destination === dest.id) {
-        name = dest.name;
-        description = dest.description;
-      }
-    }
-    return { name, description };
-  };
-  const destinationNameAndDecription = getDestinationNameAndDecription();
 
   const createEditOfferTemplate = () => {
     if (mockOffersByType) {
@@ -56,7 +42,7 @@ const editPointTemplate = (point) => {
     <option value="${city}" ${selectedCity === city ? 'selected' : ''}></option>
        `).join('');
 
-  const destListTemplate = createDestinationListTemplate(destinationNameAndDecription.name);
+  const destListTemplate = createDestinationListTemplate(destinations[destination].name);
 
   return (`
     <form class="event event--edit" action="#" method="post">
@@ -67,34 +53,34 @@ const editPointTemplate = (point) => {
            <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
          </label>
          <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-     
+
          <div class="event__type-list">
            <fieldset class="event__type-group">
              <legend class="visually-hidden">Event type</legend>
-     
+
              ${typesTemplate}
            </fieldset>
          </div>
        </div>
-     
+
        <div class="event__field-group  event__field-group--destination">
          <label class="event__label  event__type-output" for="event-destination-1">
          ${type}
          </label>
-         <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinationNameAndDecription.name}" list="destination-list-1">
+         <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${destinations[destination].name}" list="destination-list-1">
          <datalist id="destination-list-1">
          ${destListTemplate}
          </datalist>
        </div>
-     
+
        <div class="event__field-group  event__field-group--time">
          <label class="visually-hidden" for="event-start-time-1">From</label>
-         <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${humanizeDateDDMMYYHHmm(dateFrom)}">
+         <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${humanizeDateDDMMYYHHmm(dateTo)}">
          —
          <label class="visually-hidden" for="event-end-time-1">To</label>
-         <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${humanizeDateDDMMYYHHmm(dateTo)}">
+         <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${humanizeDateDDMMYYHHmm(dateFrom)}">
        </div>
-     
+
        <div class="event__field-group  event__field-group--price">
          <label class="event__label" for="event-price-1">
            <span class="visually-hidden">Price</span>
@@ -102,7 +88,7 @@ const editPointTemplate = (point) => {
          </label>
          <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
        </div>
-     
+
        <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
        <button class="event__reset-btn" type="reset">Delete</button>
        <button class="event__rollup-btn" type="button">
@@ -112,15 +98,15 @@ const editPointTemplate = (point) => {
      <section class="event__details">
        <section class="event__section  event__section--offers">
          <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-     
+
          <div class="event__available-offers">
          ${offersTemplate}
          </div>
        </section>
-     
+
        <section class="event__section  event__section--destination">
          <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-         <p class="event__destination-description">${destinationNameAndDecription.description}</p>
+         <p class="event__destination-description">${destinations[destination].description}</p>
        </section>
      </section>
     </form>
