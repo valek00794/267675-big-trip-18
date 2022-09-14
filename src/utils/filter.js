dbaab00f370ec7782d470003b6aca6b2f059e7b5
Valentin <valentin_00794@bk.ru>
2022-09-14 18:49:36 +0300

    Фикс по тз
    -начало точки в прошлом, конец в будущем должно быть во всех 3х фильтрах

--- src/utils/filter.js
+++ src/utils/filter.js
@@ -3,8 +3,8 @@ import dayjs from 'dayjs';
 
 const filter = {
   [FilterType.ALL]: (points) => points,
-  [FilterType.FUTURE]: (points) => points.filter((point) => dayjs().isBefore(point.dateFrom) || dayjs().isBefore(point.dateFrom)),
-  [FilterType.PAST]: (points) => points.filter((point) => dayjs().isAfter(point.dateFrom) && dayjs().isAfter(point.dateFrom)),
+  [FilterType.FUTURE]: (points) => points.filter((point) => dayjs().isBefore(point.dateFrom) || dayjs().isBefore(point.dateTo)),
+  [FilterType.PAST]: (points) => points.filter((point) => dayjs().isAfter(point.dateFrom)),
 };
 
 export { filter };
