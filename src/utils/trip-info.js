a6157bb81b037aaeb39cd94e8673770c5df1f4b4
Valentin <valentin_00794@bk.ru>
2022-09-20 13:32:54 +0300

    Фикс с использванием методов

--- src/utils/trip-info.js
+++ src/utils/trip-info.js
@@ -3,18 +3,17 @@ import dayjs from 'dayjs';
 const getTripInfo = (pointsModel) => {
   const pointsSequence = pointsModel.points.slice(0);
   let tripCost = 0;
-  pointsSequence.sort((a, b) => dayjs(a.dateFrom).isAfter(b.dateFrom) ? 1 : -1).forEach((point) => {
-    pointsModel.offers.forEach((offersByType) => {
-      if (offersByType.type === point.type) {
-        offersByType.offers.forEach((offerByType) => {
-          if (point.offers.includes(offerByType.id)) {
-            tripCost += Number(offerByType.price);
-          }
-        });
-      }
+  pointsSequence.sort((a, b) => dayjs(a.dateFrom).isAfter(b.dateFrom) ? 1 : -1)
+    .forEach((point) => {
+      const offersByType = pointsModel.offers.find((offer) => offer.type === point.type).offers;
+      offersByType.forEach((offerByType) => {
+        if (point.offers.includes(offerByType.id)) {
+          tripCost += Number(offerByType.price);
+        }
+      });
+      tripCost += Math.abs(Number(point.basePrice));
     });
-    tripCost += Math.abs(Number(point.basePrice));
-  });
+
   return { pointsSequence, tripCost };
 };
 
